b23179ccf121eecab1192cf97753338fecee42aa
Çağkan Çağlar <cagkancaglar@hotmail.com>
2022-11-27 17:59:34 +0300

    controller

--- src/js/index.js
+++ src/js/index.js
@@ -5,12 +5,27 @@
 // https://api.themoviedb.org/3/search/movie?api_key=289d668a77fbdec88cb12f2e41cfc474&language=en-US&query=abc&page=1&include_adult=false
 
 
-
-// model-view-controller
-
 import Search from "./models/search";
 
-const search = new Search("abc");
+const state = {};
 
-console.log(search);
-search.getResults();
+const searchController = async () => {
+    const keyword = document.getElementById("txt-keyword").value;
+
+    if(keyword){
+        state.search = new Search(keyword);
+
+        await state.search.getResults();
+
+        console.log(state.search.data.results);
+    }else {
+        alert("Anahtar kelime girmelisiniz!")
+    }
+}
+
+document.getElementById("form-search").addEventListener("submit", function(e){
+    e.preventDefault();
+    searchController();
+    console.log("form submitted");
+})
+
--- src/js/models/search.js
+++ src/js/models/search.js
@@ -8,7 +8,6 @@ export default class Search {
         const base_url = "https://api.themoviedb.org/3";
     
         const response = await fetch(`${base_url}/search/movie?api_key=${api_key}&page=1&query=${this.keyword}`);
-        this.data = await response.json();    
-    
+        this.data = await response.json();
     }
 }
